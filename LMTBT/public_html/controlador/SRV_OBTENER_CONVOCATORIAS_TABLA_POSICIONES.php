158360ed3eb40d9c81bc1b7c7b240a00baef0674
Dark_ <Dark_@DESKTOP-GPUB8MJ>
2018-01-24 17:46:49 -0600

    parche tabla convocatorias

--- LMTBT/public_html/controlador/SRV_OBTENER_CONVOCATORIAS_TABLA_POSICIONES.php
+++ LMTBT/public_html/controlador/SRV_OBTENER_CONVOCATORIAS_TABLA_POSICIONES.php
@@ -5,7 +5,7 @@
     $conn = $db->getConnection();
     
     $tipo = $_POST['tipo'];
-    $query="SELECT ID_CONVOCATORIA,NOMBRE_TORNEO FROM convocatoria";
+    $query="SELECT ID_CONVOCATORIA,NOMBRE_TORNEO FROM convocatoria WHERE CURDATE()>FECHA_CIERRE_CONVOCATORIA";
    
     if($tipo==1){$query="SELECT ID_CONVOCATORIA,NOMBRE_TORNEO FROM convocatoria WHERE ESTADO='ACTIVO'";}
     else if($tipo==2){$query="SELECT ID_CONVOCATORIA,NOMBRE_TORNEO FROM convocatoria WHERE ESTADO='INACTIVO' AND CURDATE()>FECHA_CIERRE_CONVOCATORIA";}
