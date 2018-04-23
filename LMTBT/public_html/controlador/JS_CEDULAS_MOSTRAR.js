b1ede1db0f36834c776f6cc4244682d3927c4ca8
MAURICIOARMANDO <MAURICIOARMANDO@maurice1197>
2018-04-22 23:45:47 -0500

    Refactorizacion total mawi v6

--- LMTBT/public_html/controlador/JS_CEDULAS_MOSTRAR.js
+++ LMTBT/public_html/controlador/JS_CEDULAS_MOSTRAR.js
@@ -1,10 +1,10 @@
-//fucion que guarda las variables recibidos del boton
+//fucion que guarda las variables recibidos del boton para crear un roster
 function guardar_cedula(id_equipo_1, id_equipo_2, id_rol_juego, id_convocatoria) {
     //se utiliza sessionstorage para almacenar las variables y pasarlas a otro javascript
     sessionStorage.setItem("id_equipo_1", id_equipo_1);
     sessionStorage.setItem("id_equipo_2", id_equipo_2);
     sessionStorage.setItem("id_rol_juego", id_rol_juego);
     sessionStorage.setItem("id_convocatoria", id_convocatoria);
-//redireccionamos a la pagina donde se utilizaran las variables que guarsaron los sessionstorage
+//redireccionamos a la pagina donde se utilizaran las variables que guarsaron los sessionstorage 
     location.href = "../vista/CEDULAS_FORMULARIO.html";
 }
